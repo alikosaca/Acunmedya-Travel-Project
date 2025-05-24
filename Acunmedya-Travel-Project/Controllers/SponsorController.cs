75cf5614edbb1a85d2bd1d322e69e830bdc5ee95
ali <137689491+alikosaca@users.noreply.github.com>
2025-05-24 16:03:32 +0300

    Add Sponsor and Testimonial modules with full CRUD operations and controllers
    - Implemented Index, Add, Update, and Delete functionality for Sponsor
    - Implemented Index, Add, Update, and Delete functionality for Testimonial
    - Added SponsorController and TestimonialController

---
+++ Acunmedya-Travel-Project/Controllers/SponsorController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Acunmedya_Travel_Project.Entities;
+using Acunmedya_Travel_Project.Concrete;
+using Newtonsoft.Json.Linq;
+
+namespace Acunmedya_Travel_Project.Controllers
+{
+    public class SponsorController : Controller
+    {
+        Acunmedya_Travel_Project.Concrete.Context _context = new Acunmedya_Travel_Project.Concrete.Context();
+        // GET: Sponsor
+        public ActionResult Index()
+        {
+            var values = _context.Sponsors.ToList();
+            return View(values);
+        }
+        public ActionResult AddSponsors()
+        {
+            //ekleme işlemi
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddSponsors(Sponsor _sponsor)
+        {
+            _context.Sponsors.Add(_sponsor);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteSponsors(int id)
+        {
+            var values = _context.Sponsors.Find(id);
+            _context.Sponsors.Remove(values);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UpdateSponsors(int id)
+        {
+            var values = _context.Sponsors.Find(id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult UpdateSponsors(Sponsor _sponsor)
+        {
+            var values = _context.Sponsors.Find(_sponsor.SponsorID);
+            values.ImageUrl = _sponsor.ImageUrl;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Acunmedya-Travel-Project/Controllers/TestimonialController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Acunmedya_Travel_Project.Entities;
+using Acunmedya_Travel_Project.Concrete;
+
+namespace Acunmedya_Travel_Project.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        Acunmedya_Travel_Project.Concrete.Context _context = new Acunmedya_Travel_Project.Concrete.Context();
+        public ActionResult Index()
+        {
+            var values = _context.Testimonials.ToList();
+            return View(values);
+        }
+        public ActionResult AddTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddTestimonial(Testimonial _testimonial)
+        {
+            _context.Testimonials.Add(_testimonial);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UpdateTestimonial(int id)
+        {
+            var values = _context.Testimonials.Find(id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult UpdateTestimonial(Testimonial _testimonial)
+        {
+            var values = _context.Testimonials.Find(_testimonial.TestimonialID);
+            values.Name = _testimonial.Name;
+            values.Surname = _testimonial.Surname;
+            values.ImageUrl = _testimonial.ImageUrl;
+            values.Description = _testimonial.Description;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteTestimonial(int id)
+        {
+            var values = _context.Testimonials.Find(id);
+            _context.Testimonials.Remove(values);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
